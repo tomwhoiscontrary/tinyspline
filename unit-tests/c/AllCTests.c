5c596ba14148ad1362d37f371087004066a83f87
Marcel <github@retux.de>
2016-01-26 12:25:34 +0100

    add more tests

--- unit-tests/c/AllCTests.c
+++ unit-tests/c/AllCTests.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 CuSuite* get_fequals_suite();
+CuSuite* get_default_suite();
+CuSuite* get_free_suite();
 
 void main()
 {
@@ -9,9 +11,14 @@ void main()
     CuSuite* suite = CuSuiteNew();
 
     CuSuiteAddSuite(suite, get_fequals_suite());
+    CuSuiteAddSuite(suite, get_default_suite());
+    CuSuiteAddSuite(suite, get_free_suite());
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
     CuSuiteDetails(suite, output);
     printf("%s\n", output->buffer);
+
+    CuStringDelete(output);
+    CuSuiteDelete(suite);
 }
